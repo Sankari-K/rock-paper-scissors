d74669ea0c49ce78890db632c600797cd9d1faa0
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-01 09:57:56 +0530

    Add logic for one game

--- javascript.js
+++ javascript.js
@@ -81,43 +81,67 @@ let games_played = 0;
 let won = 0;
 let lost = 0;
 const user_choice = document.querySelectorAll('.big .symbol');
- 
+let tryAgain = document.querySelector('.try-again');
+const computerChoice = document.querySelector('.computer-choice');
+const finalResult = document.querySelector('.final-result');
+let homePage = document.querySelector('.home');
+
 user_choice.forEach((button) => {
     button.addEventListener('click', 
     function () {
         games_played += 1;
         const tot_games = document.querySelector('#tot');
-        tot_games.textContent = games_played;
+        tot_games.textContent = `Total Games: ${games_played}`;
 
-        let comp_choice = computerPlay();
-        const computer_choice = document.querySelector('.computer-choice');
-        computer_choice.textContent = `The computer chose: ${comp_choice}`;
+        let compChoice = computerPlay();
+        computerChoice.textContent = `The computer chose: ${compChoice}`;
 
-        let player_choice = button.id;
+        let playerChoice = button.id;
         
-        let outcome = playRound(player_choice, comp_choice);
+        let outcome = playRound(playerChoice, compChoice);
         const roundResult = document.querySelector('.round-result');
-        if (outcome === 1)
-        {
+
+        const wonGames = document.querySelector('#won');;
+        const lostGames = document.querySelector('#lost');;
+        if (outcome === 1) {
             won += 1;
-            const wonGames = document.querySelector('#won');
-            wonGames.textContent = won;
-            roundResult.textContent = `${player_choice} beats ${comp_choice}. You win the round!`
+            wonGames.textContent = `You won: ${won}`;
+            roundResult.textContent = `${playerChoice} beats ${compChoice}. You win the round!`
         }
-        else if (outcome === -1)
-        {
+        else if (outcome === -1) {
             lost += 1;
-            const lostGames = document.querySelector('#lost');
-            lostGames.textContent = lost;
-            roundResult.textContent = `${comp_choice} beats ${player_choice}. You lost the round!`
+            lostGames.textContent = `Computer won: ${lost}`;
+            roundResult.textContent = `${compChoice} beats ${playerChoice}. You lost the round!`
         }
         else {
             roundResult.textContent = `This round is a tie!`
         }
+
+        if (won === 5 || lost === 5) {
+            finalResult.textContent = won === 5? "You won the game! :)" : "You lost the game! :(";
+            lost === 5 ? finalResult.style.color = '#ff5c5c': finalResult.style.color = '#98ff98';
+            won = 0;
+            lost = 0;
+            games_played = 0;
+            roundResult.textContent = '';
+            wonGames.textContent = '';
+            lostGames.textContent = ''; 
+            tot_games.textContent = '';
+
+            tryAgain.textContent = 'Play again?';
+            homePage.textContent = 'Main menu';
+            /* user_choice.forEach((button) => {
+                button.removeEventListener('click');
+            }); */
+        }
     });
 }); 
 
-console.log(user_choice);
-user_choice.forEach((button) => {
-    button.addEventListener('click', function () {console.log("hu"); })
-}); 
+tryAgain.addEventListener('click', 
+    function() {
+        console.log('clicked');
+        computerChoice.textContent = '';
+        finalResult.textContent = '';
+        tryAgain.textContent = '';
+        homePage.textContent = '';
+    })
